ff1698a4490af6790936ca9e5ddfc182807ea751
zjleblanc <zjleblanc3@gmail.com>
2023-05-05 09:28:51 -0700

    v1.0.8 - refactor mkreadme functions

--- ansiblebutler/roles/_mkreadme.py
+++ ansiblebutler/roles/_mkreadme.py
@@ -9,39 +9,29 @@ def mk_readme(role: str):
     "role_name": role_name,
     "meta": {
       "galaxy_info": {}
-    },
-    "defaults": {}
+    }
   }
   template_vars.update(get_yaml_dict(role + '/meta/main.yml', 'meta'))
-  template_vars.update(get_defaults(role + '/defaults/main.yml'))
-  template_vars.update(get_required_vars(role + '/defaults/main.yml', 'required_vars'))
+  template_vars.update(get_yaml_dict(role + '/defaults/main.yml', 'defaults'))
   template_vars.update(get_yaml_dict(role + '/vars/main.yml', 'vars'))
   template_vars.update(get_yaml_dict(role + '/handlers/main.yml', 'handlers'))
+  template_vars.update(get_required_vars(role + '/defaults/main.yml', 'required_vars'))
   
   template = get_template('README.md.j2', TEMPLATE_DIR)
   template.stream(template_vars).dump(role + '/README-butler.md')
 
-def get_defaults(yml):
-  defaults = get_yaml_dict(yml)
-  comments = parse_comment_line_endings(yml)
-  return { "defaults": defaults, "default_comments": comments }
-
-def get_yaml_dict(yml, key=None) -> dict:
+def get_yaml_dict(yml, key) -> dict:
   if os.path.isfile(yml):
     parsed = parse_yml(yml) or {}
+    comments = parse_comment_line_endings(yml)
   else:
-    return {}
+    parsed = {}
+    comments = {}
 
-  if key:
-    return { key: parsed }
-  return parsed or {}
+  return { key: parsed, key + "_comments": comments }
 
-def get_required_vars(yml, key=None) -> dict:
+def get_required_vars(yml, key) -> dict:
   if os.path.isfile(yml):
-    parsed = parse_comment_lines(yml) or {}
+    return { key: parse_comment_lines(yml) or {} }
   else:
-    return {}
-
-  if key:
-    return { key: parsed }
-  return parsed or {}
+    return { key: {} }
