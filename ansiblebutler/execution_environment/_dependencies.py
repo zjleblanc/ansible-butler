eff22ee619312728a93aa6d80a5c149be9a8b15c
zjleblanc <zjleblanc3@gmail.com>
2023-08-24 15:58:44 -0500

    refactor ee deps to handle ee definitions

--- ansiblebutler/execution_environment/_dependencies.py
+++ ansiblebutler/execution_environment/_dependencies.py
@@ -1,4 +1,5 @@
 import json
+import os
 from ..common import load_yml
 from ..common.dependencies import DependencyResolver
 
@@ -17,10 +18,32 @@ def pip_dry_run(requirements: list[str]):
   except AssertionError:
     pass # Ignore context related error using CLI from code
 
+def get_collections(definition_path) -> list[str]:
+  definition = load_yml(definition_path)
+  # process requirements.yml
+  if 'collections' in definition:
+    return definition['collections']
+  # process ee v3
+  if int(definition.get('version', -1)) == 3 and 'dependencies' in definition:
+    galaxy = definition['dependencies'].get('galaxy', None)
+    if not galaxy:
+      return []
+    if isinstance(galaxy, str):
+      return get_collections(os.path.dirname(definition_path) + '/' + galaxy)
+    if isinstance(galaxy, dict):
+      return definition['dependencies']['galaxy'].get('collections', [])
+  # process ee <= v2
+  dependencies = definition.get('dependencies', {})
+  if dependencies.get('galaxy', None):
+    return get_collections(os.path.dirname(definition_path) + '/' + dependencies['galaxy'])
+  # unexpected file type
+  print(f"[WARNING]Found unexpected contents in {definition_path} - please confirm this is a requirements file or execution environment definition")
+  return []
+
 def desc_deps(definition_path: str, config: dict):
   resolver = DependencyResolver()
-  definition = load_yml(definition_path)
-  resolver.resolve(definition['collections'])
+  collections = get_collections(definition_path)
+  resolver.resolve(collections)
   print("Dependencies Identified")
   print("-----------------------")
   print(json.dumps(resolver.dependencies, indent=2))
